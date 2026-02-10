121126932f15fa47e67598a218e638dfb4a1435c
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-10 11:06:19 -0300

    Fix: implementado os primeiros testes com Testing Library

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,1 +1,9 @@
-import App from "../../App";
+import { render, screen } from "@testing-library/react";
+import PostComments from ".";
+
+describe('Testes para o componente principal', () => {
+    test('Deve renderizar corratamente', () => {
+        render(<PostComments />)
+        expect(screen.getByText('comentar')).toBeInTheDocument()
+    })
+})
