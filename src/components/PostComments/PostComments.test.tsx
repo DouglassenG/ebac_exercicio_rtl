ef5f170572a6e430b18e0aa88a2b1e23f6bd377c
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-10 12:26:58 -0300

    Tests: Implementando as configurações iniciais

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import PostComments from ".";
 
 describe('Testes para o componente principal', () => {
@@ -6,4 +6,12 @@ describe('Testes para o componente principal', () => {
         render(<PostComments />)
         expect(screen.getByText('Comentar')).toBeInTheDocument()
     })
+
+    test('Deve adicionar 2 comentários', () => {
+        render(<PostComments />)
+        fireEvent.change(screen.getByTestId('campo-comentarario'),
+            { target: { value: 'Primeiro comentário' } })
+    })
+    fireEvent.click(screen.getByTestId('btn-comentar'))
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument()
 })
