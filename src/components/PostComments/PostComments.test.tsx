df51767dc3582612267d295f792cd92ff7e3435c
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-12 11:54:18 -0300

    Test update

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,12 +2,16 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import PostComments from ".";
 
 describe("Testes para o componente principal", () => {
-    it('Deve renderizar corretamente o componente', () => {
-        render(<PostComments />);
-        expect(screen.getByTestId("Comentar")).toBeInTheDocument();
-  })  
-
-  it('Deve adicionar dois comentários', () => {
+  it("Deve renderizar corretamente o componente", () => {
     render(<PostComments />);
-})
+    expect(screen.getByTestId("Comentar")).toBeInTheDocument();
+  });
 
+  it("Deve adicionar dois comentários", () => {
+    render(<PostComments />);
+    fireEvent.change(screen.getByTestId("campo-comentario"), {
+      target: { value: "Primeiro comentário" },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentar"));
+  });
+});
