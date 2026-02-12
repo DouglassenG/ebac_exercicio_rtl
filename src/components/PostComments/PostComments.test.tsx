4f14ac2565e1772e9e6451d2a87ecf095837a953
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-12 11:12:52 -0300

    Implementando os primeiros testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,14 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import PostComments from ".";
 
-describe('Testes para o componente principal', () => {
-    test('Deve renderizar corratamente', () => {
-        render(<PostComments />)
-        expect(screen.getByText('Comentar')).toBeInTheDocument()
-    })
-
-    it('Deve adicionar 2 comentários', () => {
+describe("Testes para o componente principal", () => {
+  test("Deve renderizar corratamente", () => {
     render(<PostComments />);
-
-    fireEvent.change(screen.getByTestId('campo-comentario'), { target: { value: 'Primeiro comentário' } });
-})
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+});
